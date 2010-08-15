50b4839e29ef72e057f3c4a209f5ad065d9b113b
gloupsy <gloupsy@26ce8c4a-5add-457f-a3ae-dfd0da483ec7>
2010-08-15 21:14:56 +0000

    Guillaume : fichiers weapons
    git-svn-id: http://subversion.assembla.com/svn/superbabar@77 26ce8c4a-5add-457f-a3ae-dfd0da483ec7

---
+++ src/weapons.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <SDL/SDL.h>
+#include <stdint.h>
+
+#include "sprites.h"
+#include "globals.h"
+#include "weapons.h"
+
+Weapon::Weapon()
+{
+    m_weapon_type = GUN;
+    m_reload_time = 2;
+}
+
+Weapon::Weapon(weapon_type type)
+{
+    m_weapon_type = type;
+
+    switch m_weapon_type {
+        case GUN :
+            m_reload_time = 2;
+            break;
+        case MACHINEGUN:
+            m_reload_time = 1;
+            break;
+        case SHUTGUN:
+            m_reload_time = 5;
+            break;
+    }
+}
+
+Weapon::~Weapon()
+{
+
+}
+
+Weapon::fire(SDL_Rect pos, horizontal h, vertical v)
+{
+    switch m_weapon_type {
+        case GUN :
+            Projectile * proj;
+            proj = new Projectile(pos, h, v, (h-1)*PROJ_SPEED, (v-1)*PROJ_SPEED);
+            break;
+        case MACHINEGUN:
+            Projectile * proj;
+            proj = new Projectile(pos, h, v, (h-1)*PROJ_SPEED, (v-1)*PROJ_SPEED);
+            break;
+        case SHUTGUN:
+            /*uint32_t x[5];
+            uint32_t y[5];
+            Projectile * proj[5];
+            if(h&&v) { /* le tir se fait en diagonale *//*
+
+            }
+            else if(h) {
+
+            }
+            else {
+
+            }
+            for(int i = 0;i<5;i++) {
+                proj[i] = new Projectile(pos, h, v, (h-1)*x[i], (v-1)*y[i]);
+            }*/
+            Projectile * proj;
+            proj = new Projectile(pos, h, v, (h-1)*PROJ_SPEED, (v-1)*PROJ_SPEED);
+            break;
+    }
+}
---
+++ src/weapons.h
@@ -0,0 +1,27 @@
+#ifndef WEAPONS_H_INCLUDED
+#define WEAPONS_H_INCLUDED
+
+#include "sprites.h"
+#include "globals.h"
+
+#define PROJ_LIFE_SPAN 15   /* Durée de vie d'un projectile */
+#define PROJ_SPEED 30       /* Vitesse des projectiles */
+
+enum weapon_type {
+    GUN, MACHINEGUN, SHUTGUN
+};
+
+class Weapon
+{
+    private:
+        weapon_type m_weapon_type;              /* type de l'arme */
+        uint32_t m_reload_time;                 /* temps entre deux tirs */
+    public:
+        Weapon();
+        Weapon(weapon_type type);
+        ~Weapon();
+        void fire(SDL_Rect pos, horizontal h, vertical v);    /* crée les projectiles de l'arme */
+        uint32_t reload_time();                 /* accesseur */
+};
+
+#endif // WEAPONS_H_INCLUDED
