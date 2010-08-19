8e7d1bd996eb99cc35051e2e5678484caef5c2a2
gloupsy <gloupsy@26ce8c4a-5add-457f-a3ae-dfd0da483ec7>
2010-08-19 09:57:26 +0000

    pour que nalou puisse tester
    git-svn-id: http://subversion.assembla.com/svn/superbabar@94 26ce8c4a-5add-457f-a3ae-dfd0da483ec7

--- editor/main.cpp
+++ editor/main.cpp
@@ -11,46 +11,46 @@
 
 int main(int argc, char *argv[])
 {
-	int choice = 0;
-	std::string file_name, background_name;
-	bool leave = false;
+	//~ int choice = 0;
+	//~ std::string file_name, background_name;
+	//~ bool leave = false;
 	
-	while (choice < 1 || choice > 4){
-		std::cout << "Editeur de niveaux SUPERBARAR" << std::endl;
-		std::cout << "Que voulez-vous faire?" << std::endl;
-		std::cout << "1. Nouveau niveau" << std::endl;
-		std::cout << "2. Charger un niveau" << std::endl;
-		std::cout << "3. Quitter" << std::endl;       
-		std::cout << "4. Défaut (pour les tests)" << std::endl;       
-		std::cin >> choice;
-	}
-	switch (choice) {
-	case 1:
-		std::cout << "Indiquez le chemin/le nom du fichier à créer" << std::endl;
-		std::cin >> file_name;
-		std::cout << "Indiquez le chemin/le nom du fond d'écran du niveau" << std::endl;
-		std::cin >> background_name;
-		new_level_file(file_name, background_name);
-		break;
-	case 2:
-		std::cout << "Indiquez le chemin/le nom du fichier à charger" << std::endl;
-		std::cin >> file_name;
-		break;
-	case 3:
-		leave = true;
-		break;                                                               
+	//~ while (choice < 1 || choice > 4){
+		//~ std::cout << "Editeur de niveaux SUPERBARAR" << std::endl;
+		//~ std::cout << "Que voulez-vous faire?" << std::endl;
+		//~ std::cout << "1. Nouveau niveau" << std::endl;
+		//~ std::cout << "2. Charger un niveau" << std::endl;
+		//~ std::cout << "3. Quitter" << std::endl;       
+		//~ std::cout << "4. Défaut (pour les tests)" << std::endl;       
+		//~ std::cin >> choice;
+	//~ }
+	//~ switch (choice) {
+	//~ case 1:
+		//~ std::cout << "Indiquez le chemin/le nom du fichier à créer" << std::endl;
+		//~ std::cin >> file_name;
+		//~ std::cout << "Indiquez le chemin/le nom du fond d'écran du niveau" << std::endl;
+		//~ std::cin >> background_name;
+		//~ new_level_file(file_name, background_name);
+		//~ break;
+	//~ case 2:
+		//~ std::cout << "Indiquez le chemin/le nom du fichier à charger" << std::endl;
+		//~ std::cin >> file_name;
+		//~ break;
+	//~ case 3:
+		//~ leave = true;
+		//~ break;                                                               
 
-	case 4:
-		file_name = "defaut";
-		break;
-	default:
-		std::cout << "probleme dans le switch du main.cpp de l'editeur" << std::endl;
-		break;
-	}
-	if (leave)
-		return 0;
+	//~ case 4:
+		//~ file_name = "defaut";
+		//~ break;
+	//~ default:
+		//~ std::cout << "probleme dans le switch du main.cpp de l'editeur" << std::endl;
+		//~ break;
+	//~ }
+	//~ if (leave)
+		//~ return 0;
 	
-	Editor editor(file_name);
+	Editor editor("test.lvl");
 	editor.edit();
 	return 0;
 }    
