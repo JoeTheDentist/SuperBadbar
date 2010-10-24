c7c732dd83292edd94a486852e89230d2284594c
Gloupsy <Gloupsy@26ce8c4a-5add-457f-a3ae-dfd0da483ec7>
2010-10-24 11:12:48 +0000

    ajout du fichier SOund
    git-svn-id: http://subversion.assembla.com/svn/superbabar@199 26ce8c4a-5add-457f-a3ae-dfd0da483ec7

---
+++ src/sound/sound.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include "sound.h"
+#include "../sound/sound_manager.h"
+
+Sound::Sound() 
+{
+	
+}
+
+Sound::~Sound()
+{
+	
+}
---
+++ src/sound/sound.h
@@ -0,0 +1,16 @@
+#ifndef _SOUND_
+#define _SOUND_
+
+
+class Sound_manager;
+
+class Sound {
+protected:
+	static Sound_manager m_sound_manager;
+public:
+	Sound();
+	~Sound();
+};
+
+
+#endif
