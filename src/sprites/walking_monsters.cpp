2eb113dfbbce59b02a4b19e7df4bddf8929e7733
Gloupsy <Gloupsy@26ce8c4a-5add-457f-a3ae-dfd0da483ec7>
2010-11-27 10:57:06 +0000

    git-svn-id: http://subversion.assembla.com/svn/superbabar@240 26ce8c4a-5add-457f-a3ae-dfd0da483ec7

--- src/sprites/walking_monsters.cpp
+++ src/sprites/walking_monsters.cpp
@@ -18,24 +18,24 @@ Walking_monster::Walking_monster(Sound_manager *sound_manager, Analyser *analyse
 {
 	m_nom = analyserLevel->read_string();
 	Analyser analyserMonster;
-	
+
 	// donnees contenues dans le level
 	m_pos.x = analyserLevel->read_int();
 	m_pos.y = analyserLevel->read_int();
 	m_area_begin = analyserLevel->read_uint32_t();
 	m_area_end = analyserLevel->read_uint32_t();
 	PRINT_DEBUG(1, "%d %d \n", m_area_begin, m_area_end);
-	
+
 	// donnees propres a la nature du monstre (a chercher dans le bestiaire)
 	analyserMonster.open((MONSTERS_STATS_R + m_nom + MONSTERS_EXT).c_str());
 	m_life = analyserMonster.read_int();
 	m_speed_def = analyserMonster.read_int();
 	analyserMonster.close();
-	
+
 	// images
     std::string pic_monsters_rep = PIC_MONSTERS_R;
 	Pictures_container pictures_container;
-	
+
 	m_pics[0][0] = pictures_container.load_BMP((pic_monsters_rep+ m_nom + "_left_1" +PICS_EXT).c_str());
 	m_pics[0][1] = pictures_container.load_BMP((pic_monsters_rep+ m_nom + "_left_2" +PICS_EXT).c_str());
 	m_pics[0][2] = pictures_container.load_BMP((pic_monsters_rep+ m_nom + "_left_3" +PICS_EXT).c_str());
@@ -46,7 +46,7 @@ Walking_monster::Walking_monster(Sound_manager *sound_manager, Analyser *analyse
 	m_pics[1][3] = pictures_container.load_BMP((pic_monsters_rep+ m_nom + "_right_2" +PICS_EXT).c_str());
 	m_speed.x = m_speed_def;
 	m_horizontal = RIGHT;
-	
+
 	m_pos.w = m_pics[0][0]->w;
 	m_pos.h = m_pics[0][0]->h;
 //~ 	for(int i = 0;i<2;i++) {
