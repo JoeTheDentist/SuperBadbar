dade2daef3003a6d68889837ccba1cf713c6b444
gloupsy <gloupsy@26ce8c4a-5add-457f-a3ae-dfd0da483ec7>
2010-12-22 13:01:48 +0000

    encore oubli de ficheirs
    git-svn-id: http://subversion.assembla.com/svn/superbabar@292 26ce8c4a-5add-457f-a3ae-dfd0da483ec7

---
+++ src/items/weapons_armory.cpp
@@ -0,0 +1,94 @@
+/**
+ * 	@file weapons_armory.cpp
+ * 	@brief Implémentation de la classe Weapons_armory
+ *
+ * 	@author Guillaume Bérard & Benoit Morel
+ * 	@date decembre 2010
+ *
+ */
+
+#include "weapons_armory.h"
+#include "../util/debug.h"
+#include "../items/weapons.h"
+#include "../items/gun.h"
+#include "../items/shotgun.h"
+#include "../items/machinegun.h"
+#include "../video/pictures_container.h"
+#include "../sound/sound_manager.h"
+
+
+Weapons_armory::Weapons_armory(Pictures_container *pictures_container, Sound_manager *sound_manager)
+{
+	PRINT_CONSTR(2, "Construction de Weapons_armory");
+	m_pictures_container = pictures_container;
+	m_sound_manager = sound_manager;
+	Gun *gun = new Gun(m_pictures_container, sound_manager);
+	m_armory.push_back(gun);
+	m_current_weapon = m_armory.begin();
+}
+
+Weapons_armory::~Weapons_armory()
+{
+	
+}
+
+Weapon *Weapons_armory::get_current_weapon()
+{
+	return *m_current_weapon;
+}
+
+void Weapons_armory::add_weapon(weapon_type type)
+{
+	std::list<Weapon *>::iterator it; 
+	// Positionnement dans la liste
+	for (it = m_armory.begin(); it != m_armory.end() && (*it)->type_of_weapon() < type; it++) {}
+	// Cas où l'arme est déjà présente
+	if (it != m_armory.end()) {
+		if ((*it)->type_of_weapon() == type) {
+			PRINT_DEBUG(1, "MEME TYPE!") 
+			(*it)->add_munitions();
+			return;
+		}
+	}
+	// Cas où l'arme doit etre ajoutée
+	switch (type) {
+		case GUN:
+			m_armory.insert(it,new Gun(m_pictures_container, m_sound_manager));
+			break;
+		case SHOTGUN:
+			m_armory.insert(it,new Shotgun(m_pictures_container, m_sound_manager));
+			break;
+		case MACHINEGUN:
+			m_armory.insert(it,new Machinegun(m_pictures_container, m_sound_manager));
+			break;
+		default:
+			PRINT_DEBUG(1, "type d'arme inconnu dans add_weapon de weapon_armory");
+	}
+	// Changement d'arme actuelle ou non
+	it--;
+	if ((*it)->type_of_weapon() > get_current_weapon()->type_of_weapon())
+		m_current_weapon = it;
+}
+
+void Weapons_armory::update()
+{
+	if ((*m_current_weapon)->munitions() <= 0) {
+		std::list<Weapon *>::iterator it = m_current_weapon; 
+		it++;
+		m_armory.erase(m_current_weapon);
+		m_current_weapon--;
+		if (it != m_armory.end()) {
+			m_current_weapon = it;
+		}
+	}
+}
+
+void Weapons_armory::next_weapon()
+{
+	
+}
+
+void Weapons_armory::previous_weapon()
+{
+	
+}
---
+++ src/items/weapons_armory.h
@@ -0,0 +1,79 @@
+/**
+ * 	@file weapons_armory.h
+ * 	@brief Header de la classe Weapons_armory
+ *
+ * 	@author Guillaume Bérard & Benoit Morel
+ * 	@date decembre 2010
+ *
+ */
+
+#ifndef _WEAPONS_ARMORY_
+#define _WEAPONS_ARMORY_
+
+#include <list>
+#include "../items/weapons.h"
+class Weapon;
+class Pictures_container;
+class Sound_manager;
+/**
+ * 	@class Weapons_armory
+ * 	@brief Contient et gère les armes possédées par un personnage
+ */
+class Weapons_armory {
+private:
+	Pictures_container *m_pictures_container;
+	Sound_manager *m_sound_manager;
+	std::list<Weapon*> m_armory; 
+	std::list<Weapon *>::iterator m_current_weapon;
+public:
+	/*!
+	 *	@brief Constructeur
+	 *	@param pictures_container Le gestionnaire d'images
+	 *	@param sound_manager Le gestionnaire de son
+	*/
+	Weapons_armory(Pictures_container *pictures_container, Sound_manager *sound_manager);
+
+	/*!
+	 *	@brief Destructeur
+	*/
+	~Weapons_armory();
+
+	/*!
+	 *	@brief Accesseur
+	 *	@return Un pointeur vers l'arme actuelle
+	*/
+	Weapon *get_current_weapon();
+
+	/*!
+	 *	@brief Ajout d'une arme à l'arsenal
+	 *	@param type Le type d'arme à ajouter
+	 *
+	 *	Si l'arme est déjà présente dans l'inventaire, on lui rajoute 
+	 *	des munitions. Sinon, on la crée et on l'ajoute.
+	*/
+	void add_weapon(weapon_type type);
+
+	/*!
+	 *	@brief Mise à jour de l'inventaire
+	 *
+	 *	Supprime les armes n'ayant plus de munitions
+	*/
+	void update();
+	
+	/*!
+	 *	@brief Sélectionne l'arme suivante
+	 *
+	 *	Quand on arrive à la dernière arme, on repart de la première
+	*/
+	void next_weapon();
+	
+	/*!
+	 *	@brief Sélectionne l'arme précédente
+	 *
+	 *	Quand on arrive à la première arme, on repart de la dernière
+	*/
+	void previous_weapon();
+};
+
+
+#endif
