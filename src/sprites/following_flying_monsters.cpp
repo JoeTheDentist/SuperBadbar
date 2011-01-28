f8477366f6fa36d952f98f38f5471cdd1cc0fa24
gloupsy <gloupsy@26ce8c4a-5add-457f-a3ae-dfd0da483ec7>
2011-01-28 17:59:04 +0000

    add  following_flying_monsters
    git-svn-id: http://subversion.assembla.com/svn/superbabar@367 26ce8c4a-5add-457f-a3ae-dfd0da483ec7

---
+++ src/sprites/following_flying_monsters.cpp
@@ -0,0 +1,72 @@
+/**
+ * 	@file flying_monster.cpp
+ * 	@brief Source de la classe Flying_monster
+ *
+ * 	@author Guillaume Bérard & Benoit Morel
+ * 	@date decembre 2010
+ *
+ */
+#include <iostream>
+#include <SDL/SDL.h>
+#include <stdint.h>
+
+#include "following_flying_monsters.h"
+#include "../util/debug.h"
+#include "../util/analyser.h"
+#include "babar.h"
+
+
+Following_flying_monster::Following_flying_monster(Analyser *analyserLevel)
+{
+    PRINT_CONSTR(1, "                               Construction d'un following_flying_monster")
+	m_nom = analyserLevel->read_string();
+	Analyser analyserMonster;
+
+	// donnees contenues dans le level
+	m_pos.x = analyserLevel->read_int();
+	m_pos.y = analyserLevel->read_int();
+
+	// donnees propres a la nature du monstre (a chercher dans le bestiaire)
+	analyserMonster.open((MONSTERS_STATS_R + m_nom + MONSTERS_EXT).c_str());
+	m_life = analyserMonster.read_int();
+	m_speed_def = analyserMonster.read_int();
+	analyserMonster.close();
+
+	// images
+    std::string pic_monsters_rep = PIC_MONSTERS_R;
+
+    m_animt = new Anim_table(pic_monsters_rep+m_nom+"/"+m_nom);
+
+	m_speed.x = m_speed_def;
+	m_dir = RIGHT;
+
+    m_animt->set_rect(m_pos);
+
+	m_can_fire = false;
+}
+
+Following_flying_monster::~Following_flying_monster()
+{
+
+}
+
+
+void Following_flying_monster::update_speed(Babar *babar)
+{
+	if ( (m_pos.x<babar->position().x)&&(m_dir != RIGHT) ) {
+	    m_dir = RIGHT;
+		m_speed.x = m_speed_def;
+	}
+	if ( (m_pos.x>babar->position().x)&&(m_dir != LEFT) ) {
+	    m_dir = LEFT;
+	    m_speed.x = -m_speed_def;
+	}
+
+	if ( (m_pos.y<babar->position().y) ) {
+		m_speed.y = m_speed_def;
+	}
+	if ( (m_pos.y>babar->position().y) ) {
+	    m_speed.y = -m_speed_def;
+	}
+}
+
---
+++ src/sprites/following_flying_monsters.h
@@ -0,0 +1,49 @@
+/**
+ * 	@file Following_flying_monsters.h
+ * 	@brief Header de la classe Following_flying_monsters
+ *
+ * 	@author Guillaume Bérard & Benoit Morel
+ * 	@date decembre 2010
+ *
+ */
+
+#ifndef FOLLOWING_FLYING_MONSTERS_H_INCLUDED
+#define FOLLOWING_FLYING_MONSTERS_H_INCLUDED
+
+#include "../sprites/monsters.h"
+#include "flying_monster.h"
+
+class Analyser;
+
+
+/**
+ * 	@class Following_flying_monster
+ * 	@brief Monstre qui vole et qui suit
+ *
+ */
+class Following_flying_monster : public Flying_monster {
+private:
+	int m_high_min;
+
+public:
+	/**
+	 * 	@brief Constructeur
+	 *	@param analyserLevel Analyseur se fichier
+	*/
+	Following_flying_monster(Analyser *analyserLevel);
+
+	/**
+	 * 	@brief Destructeur
+	 */
+	~Following_flying_monster();
+
+	/**
+	 * 	@brief Met à jour la vitesse du monstre
+	 *	@param babar Parametre inutilisé dans cette sous classe
+	 *
+	**/
+	void update_speed(Babar *babar);
+};
+
+
+#endif // FOLLOWING_FLYING_MONSTERS_H_INCLUDED
