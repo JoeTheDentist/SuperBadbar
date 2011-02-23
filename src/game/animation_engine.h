5d859cd101a656d59e729114c5977318e0a6d269
NalWarful <NalWarful@26ce8c4a-5add-457f-a3ae-dfd0da483ec7>
2011-02-23 12:53:23 +0000

    Debut du joueur d'animation
    git-svn-id: http://subversion.assembla.com/svn/superbabar@407 26ce8c4a-5add-457f-a3ae-dfd0da483ec7

---
+++ src/game/animation_engine.h
@@ -0,0 +1,19 @@
+/**
+ * 	@file animation_engine.h
+ * 	@brief Conteneur et joueur d'animation indépendantes
+ *
+ * 	@author Guillaume Bérard & Benoit Morel
+ * 	@date decembre 2010
+ *
+ */
+
+#ifndef _ANIMATION_ENGINE_
+#define _ANIMATION_ENGINE_
+
+class animation_engine {
+	private:
+		
+	public:
+}
+
+#enif
