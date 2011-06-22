fd156eec656b7c71f61fedcb3cf1dbcda920353c
Gloupsy <Gloupsy@26ce8c4a-5add-457f-a3ae-dfd0da483ec7>
2011-06-22 17:16:03 +0000

    plop
    git-svn-id: http://subversion.assembla.com/svn/superbabar@798 26ce8c4a-5add-457f-a3ae-dfd0da483ec7

--- src/main.cpp
+++ src/main.cpp
@@ -21,20 +21,20 @@ int main(int argc, char *argv[])
 {
 	PRINT_TRACE(1,"Lancement du jeu")
 	Game_initializer::init_game();
-	Arg_Analyser arg_analyser(argc, argv);
-	if (arg_analyser.level_specified_on()) {
-		Game *game = new Game(arg_analyser.get_level_name());
-		game->game_loop();
-		delete game;
+//~ 	Arg_Analyser arg_analyser(argc, argv);
+//~ 	if (arg_analyser.level_specified_on()) {
+//~ 		Game *game = new Game(arg_analyser.get_level_name());
+//~ 		game->game_loop();
+//~ 		delete game;
 
-	} else if(arg_analyser.menu_on()) {
+//~ 	} else if(arg_analyser.menu_on()) {
 		Main_menu *menu = new Main_menu();
 		delete menu;
-	} else {
-		Game *game = new Game("test.lvl");
-		game->game_loop();
-		delete game;
-	}
+//~ 	} else {
+//~ 		Game *game = new Game("test.lvl");
+//~ 		game->game_loop();
+//~ 		delete game;
+//~ 	}
 	Game_initializer::close_game();
 	PRINT_TRACE(1, "Fermeture du jeu")
 //~ 	Talks talks;
