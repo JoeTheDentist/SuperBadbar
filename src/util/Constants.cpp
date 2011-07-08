4aa8b848577019c449858ca4d984421bb40e2521
gloupsy <gloupsy@26ce8c4a-5add-457f-a3ae-dfd0da483ec7>
2011-07-08 16:08:45 +0000

    save
    git-svn-id: http://subversion.assembla.com/svn/superbabar@832 26ce8c4a-5add-457f-a3ae-dfd0da483ec7

--- src/util/Constants.cpp
+++ src/util/Constants.cpp
@@ -2,20 +2,29 @@
 #include "../util/Analyser.h"
 #include "../util/repertories.h"
 #include <iostream>
+
 int Constants::FULL_SCREEN = 0;
+std::string Constants::TEST_LEVEL = "test.lvl";
 
-
-
-
+#define ACO(yop) {yop}
+#define QUOTE_(yop) #yop
+#define QUOTE(yop) QUOTE_(yop)
+#define QUOTE_ACO(yop) QUOTE(ACO(yop))
+#define IDENTITY(yop) yop
+#define CONST_INIT_INT(yop) \
+if (analyser.find_string(QUOTE_ACO(yop))) { \
+	 Constants::IDENTITY(yop) = analyser.read_int(); \
+}
+	
 
 void Constants::init()
 {
 	Analyser analyser;
 	analyser.open(DATA_R + "config.cfg");
 	
-	if (analyser.find_string("#FULL_SCREEN#")) {
-		Constants::FULL_SCREEN = analyser.read_int();
-		std::cout << "FUUUUUUULL" << Constants::FULL_SCREEN << std::endl;
-	}
+//~ 	if (analyser.find_string("{FULL_SCREEN}")) {
+//~ 		Constants::FULL_SCREEN = analyser.read_int();
+//~ 	}
+	CONST_INIT_INT(FULL_SCREEN);
 	analyser.close();
 }
--- src/util/Constants.h
+++ src/util/Constants.h
@@ -1,11 +1,13 @@
 #ifndef _BABAR_CONSTANTS_H_
 #define _BABAR_CONSTANTS_H_
 
+#include <string>
 
 class Constants {
 public:
 	static void init();
 	static int FULL_SCREEN;
+	static std::string TEST_LEVEL;
 
 	
 	
