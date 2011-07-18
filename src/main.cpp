12ba861af5d89df328c97895abf5f3b9bfe41d16
NalWarful <NalWarful@26ce8c4a-5add-457f-a3ae-dfd0da483ec7>
2011-07-18 17:10:32 +0000

    main
    git-svn-id: http://subversion.assembla.com/svn/superbabar@851 26ce8c4a-5add-457f-a3ae-dfd0da483ec7

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@
 
 #include "util/debug.h"
 #include "util/repertories.h"
-<<<<<<< .mine
 #include "game/Game.h"
 #include "util/ArgAnalyser.h"
 #include "menus/MainMenu.h"
@@ -22,20 +21,8 @@
     #undef main
 #endif
 
-=======
-#include "game/Game.h"
-#include "util/ArgAnalyser.h"
-#include "menus/MainMenu.h"
-#include "game/GameInitializer.h"
-#include "video/Talks.h"
-#include "util/debug.h"
-#include "util/globals.h"
-#undef main
-
->>>>>>> .r848
 int main(int argc, char *argv[])
 {
-<<<<<<< .mine
         PRINT_TRACE(1,"Lancement du jeu");
         GameInitializer::init_game();
 #ifdef ARG_ANALYSER_ON
@@ -44,23 +31,13 @@ int main(int argc, char *argv[])
 		Game *game = new Game(arg_analyser.get_level_name());
 		game->game_loop();
 		delete game;
-=======
-        PRINT_TRACE(1,"Lancement du jeu");
-	GameInitializer::init_game();
->>>>>>> .r848
-
-<<<<<<< .mine
 	} else if(arg_analyser.menu_on()) {
                 MainMenu *menu = new MainMenu();
 		delete menu;
 	} else {
-		Game *game = new Game("test.lvl");
-=======
 		Game *game = new Game(Constants::TEST_LEVEL);
->>>>>>> .r848
 		game->game_loop();
-		delete game;
-<<<<<<< .mine
+                delete game;
 	}
 #else
 	Main_menu *menu = new Main_menu();
@@ -69,11 +46,5 @@ int main(int argc, char *argv[])
 	
         GameInitializer::close_game();
         PRINT_TRACE(1, "Fermeture du jeu");
-=======
-
-
-	GameInitializer::close_game();
-        PRINT_TRACE(1, "Fermeture du jeu");
->>>>>>> .r848
 	return 0;
 }
