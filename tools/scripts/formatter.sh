114c2b06468793573bce4348d55c050eb689a2e3
Guillaume Berard <berardgui@gmail.com>
2014-02-20 14:11:13 +0100

    Working formmatting script

--- tools/scripts/formatter.sh
+++ tools/scripts/formatter.sh
@@ -1,6 +1,18 @@
-#!/bin/sh -x
+#!/bin/sh
 # Format coding style
 # Depends on astyle binary
+
+# List of options:
+#	-s4	indentation = 4 spaces
+#	-b	breack brackets from pre-block statement
+#	-N	add extra indentation for namespaces
+#	-w	indent preprosessor multiline statements
+#	-p	insert space padding around operators
+#	-d	insert space padding around parenthesis (outside only)
+#	-Z	conserve original file's date and time modified
+#	-Q	display only formatted files
+#	-z2	end of line in linux style
+#	-r	recursive
 
 # Fail at first error
 set -e
@@ -11,10 +23,10 @@ ROOT_PATH=$(dirname $TOOLS_PATH)
 
 for f in src tools
 do
-	cd $ROOT_PATH/$f
 	for ex in cpp h
 	do
-		astyle -s4 -bNwpdZQ -r *.$ex
+		cmd="$ROOT_PATH/$f/*.$ex"
+		astyle -s4 -bNwpdZQ -z2 -r "$cmd"
 	done
 done
  
